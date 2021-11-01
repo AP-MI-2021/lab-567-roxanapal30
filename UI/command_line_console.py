9da7152eed4a104a4ca3a1d3f2c7f49dffcf1ba2
Roxana Paladi <roxana300302@gmail.com>
2021-11-01 19:48:25 +0200

    erori explicate

--- UI/command_line_console.py
+++ UI/command_line_console.py
@@ -6,63 +6,67 @@ from Logic.Mutare import mutare_locatie, verificarelocatieexistenta
 
 def command(lista):
     printMenu()
-    string=input("Dati comenzile despartite prin';' cu toate atributele necesare despartite prin ',':")
-    comenzi=string.split(";")
+    string = input("Dati comenzile despartite prin';' cu toate atributele necesare despartite prin ',':")
+    comenzi = string.split(";")
     for s in comenzi:
         subcomenzi = s.split(",")
-        if subcomenzi[0]=="add":
-            id = subcomenzi[1]
-            nume = subcomenzi[2]
-            descriere=subcomenzi[3]
-            pret = subcomenzi[4]
-            locatie = subcomenzi[5]
-            if descriere is None:
-                print("Imposibil")
-                break
-            if len(locatie)!=4:
-                print("Imposibil")
-                break
-            lista=adaugaobiect(id, nume, descriere, pret, locatie, lista)
-        elif subcomenzi[0]=="delete":
-            id=subcomenzi[1]
-            lista= stergeobiect(id, lista)
-        elif subcomenzi[0]=="modify":
+        if subcomenzi[0] == "add":
             id = subcomenzi[1]
             nume = subcomenzi[2]
             descriere = subcomenzi[3]
             pret = subcomenzi[4]
             locatie = subcomenzi[5]
             if descriere is None:
-                print("Imposibil")
+                print("Imposibil, descrierea nu poate fi null")
                 break
             if len(locatie) != 4:
-                print("Imposibil")
+                print("Imposibil, locatia trebuie sa aiba 4 caractere")
                 break
-            lista=modificaobiect(id, nume, descriere, pret, locatie)
-        elif subcomenzi[0]=="move":
-            locatieinitiala =subcomenzi[1]
+            lista = adaugaobiect(id, nume, descriere, pret, locatie, lista)
+        elif subcomenzi[0] == "delete":
+            id = subcomenzi[1]
+            lista = stergeobiect(id, lista)
+        elif subcomenzi[0] == "modify":
+            id = subcomenzi[1]
+            nume = subcomenzi[2]
+            descriere = subcomenzi[3]
+            pret = subcomenzi[4]
+            locatie = subcomenzi[5]
+            if descriere is None:
+                print("Imposibil, descrierea nu poate fi null")
+                break
+            if len(locatie) != 4:
+                print("Imposibil, locatia trebuie sa aiba 4 caractere")
+                break
+            lista = modificaobiect(id, nume, descriere, pret, locatie)
+        elif subcomenzi[0] == "move":
+            locatieinitiala = subcomenzi[1]
             locatiefinala = subcomenzi[2]
             if not verificarelocatieexistenta(locatieinitiala, lista):
-                print("Imposibil")
+                print("Imposibil, locatia trebuie sa existe in lista")
                 break
             if len(locatiefinala) != 4:
-                print("Imposibil")
+                print("Imposibil, locatia trebuie sa aiba 4 caractere")
                 break
-            lista=mutare_locatie(locatieinitiala, locatiefinala, lista)
-        elif subcomenzi[0]=="concat":
+            lista = mutare_locatie(locatieinitiala, locatiefinala, lista)
+        elif subcomenzi[0] == "concat":
             sir = subcomenzi[1]
             suma = subcomenzi[2]
-            lista=concatenare(sir, suma, lista)
-        elif subcomenzi[0]=="showall":
+            lista = concatenare(sir, suma, lista)
+        elif subcomenzi[0] == "showall":
             for obiect in lista:
                 print(toString(obiect))
+        else:
+            print("Eroare")
     return lista
 
 
+
 def printMenu():
-    print("1.add,id,nume,descriere,pret,locatie; :")
-    print("2.delete,id; :")
-    print("3.modify,id,nume,descriere,pret,locatie; :")
-    print("4.move,locatieinitiala,locatiefinala; :")
-    print("5.concat,sir,suma; :")
-    print("6.showall")
+    print("Lista cu comenzi: ")
+    print("add,id,nume,descriere,pret,locatie;")
+    print("delete,id;")
+    print("modify,id,nume,descriere,pret,locatie;")
+    print("move,locatieinitiala,locatiefinala;")
+    print("concat,sir,suma;")
+    print("showall;")
